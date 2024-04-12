bc52342b4e22f7a5bc8892448024de7947b58d94
Petrovsv84 <petrovsv84@mail.ru>
2024-04-12 20:41:01 +0300

    1

--- Application.py
+++ Application.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 
 app = Flask(__name__)
-@app.route('/')
-def print_h():
+
+@app.route('/',methods=['get','post'])#127.0.0.1:5000/
+def main():
+    if request.method == 'POST':
+        x1 = request.form.get('x1')
+        print(x1)
     return render_template("index.html")
+
+
+
 
 app.run()
