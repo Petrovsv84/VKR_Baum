668180aa9899498ae75736563e01256faa49135a
Petrovsv84 <petrovsv84@mail.ru>
2024-04-12 21:45:03 +0300

    2

--- Application.py
+++ Application.py
@@ -1,14 +1,47 @@
 from flask import Flask, render_template,request
 
-
 app = Flask(__name__)
 
 @app.route('/',methods=['get','post'])#127.0.0.1:5000/
 def main():
+    message="Введите показатели"
     if request.method == 'POST':
         x1 = request.form.get('x1')
+        x2 = request.form.get('x2')
+        x3 = request.form.get('x3')
+        x4 = request.form.get('x4')
+        x5 = request.form.get('x5')
+        x6 = request.form.get('x6')
+        x7 = request.form.get('x7')
+        x8 = request.form.get('x8')
+        x9 = request.form.get('x9')
+        x10 = request.form.get('x10')
+        x12 = request.form.get('x11')
+        x12 = request.form.get('x12')
+
+        try:
+            x1=float(x1)
+            x2=float(x2)
+            x3=float(x3)
+            x4=float(x4)
+            x5=float(x5)
+            x6=float(x6)
+            x7=float(x7)
+            x8=float(x8)
+            x9=float(x9)
+            x10=float(x10)
+            x11=float(x11)
+            x12=float(x12)
+            X_ML=[x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12]
+            y_upr_pred=predictUPR(X_ML)
+            y_rast_pred= predictRAST(X_ML)
+            message = f'Прогнозные значения для:/nМодуль упругости при растяжении, ГПа: {y_upr_pred}/n/nПрочность при растяжении, МПа: {y_rast_pred}'
+        except:
+
+
         print(x1)
-    return render_template("index.html")
+
+    return render_template("index.html", message=message)
 
 
 
---
+++ processing.py
@@ -0,0 +1,6 @@
+def predictUPR(X_ML):
+    y_upr_pred=modelUPR(X_ML)
+    return y_upr_predy_rast_pred
+def predictRAST(X_ML):
+    y_rast_pred=modelRAST(X_ML)
+    return y_rast_pred
