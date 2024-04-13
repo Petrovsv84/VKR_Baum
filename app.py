477dc97caf2023029c8370675078c78d42b0496f
Petrovsv84 <petrovsv84@mail.ru>
2024-04-13 08:43:49 +0300

    Добавлена лучшая нейросеть, подготовлено приложение, размещено в сети интернет по аддресу:https://vkr-baum-petrovsv.onrender.com Однако, ввиду непобежденной ошибки в выгрузке моделей оно не работоспособно. Отладку провести невозможно. Теоритически рабочее...

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template,request
-from processing import load_scaler,load_rast,load_upr
+from processing import load_matr, load_scaler
 
 app = Flask(__name__)
 
 @app.route('/',methods=['get','post'])#127.0.0.1:5000/
 def main():
+#    scalerdf=load_scaler()
+    pred_matr=load_matr()
     scaler=load_scaler()
-    pred_rast=load_rast()
-    pred_upr=load_upr()
 
     message="Введите показатели"
     if request.method == 'POST':
@@ -37,18 +37,15 @@ def main():
             x10=float(x10)
             x11=float(x11)
             x12=float(x12)
-            X_ML=[x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12]
-            y_upr_pred=y_upr_pred(X_ML)
-            y_rast_pred= y_rast_pred(X_ML)
-            message = f'Прогнозные значения для:/nМодуль упругости при растяжении, ГПа: {y_upr_pred}/n/nПрочность при растяжении, МПа: {y_rast_pred}'
+            X_matr=[x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12]
+            X_matr=scaler(X_matr)
+            y_matr_pred=pred_matr.prediction(X_matr)
+            message = f'Прогнозные значения для:/nСоотношение матрица-наполнитель: {y_matr_pred}'
         except:
             message="ОЙ"
 
         print(x1)
 
     return render_template("index.html", message=message)
-
-
-
 
 app.run()
--- processing.py
+++ processing.py
@@ -1,31 +1,15 @@
 import pickle
-import numpy as np
+import scikit-learn
 import pandas as pd
+import numpy as np
 
 
-def load_rast(path='models/Catboost_rast.pkl'):
-    with open(path, 'rb') as f:
-        cat_rast = pickle.load(f)
-    return cat_rast
+def load_matr(path='models/Neuro/'):
+    model = tf.keras.models.load_model(path)
+    return model
 
 
-def load_upr(path='models/Catboost_upr.pkl'):
-    with open(path, 'rb') as f:
-        cat_upr = pickle.load(f)
-    return cat_upr
-
-
-def load_scaler(path='models/Catboost_scaler.pkl'):
+def load_scaler(path='models/Scaler_neur.pkl'):
     with open(path, 'rb') as f:
         scaler = pickle.load(f)
     return scaler
-
-
-def predictUPR(X_ML, model):
-    y_upr_pred = cat_upr.predict(X_ML)
-    return y_upr_pred
-
-
-def predictRAST(X_ML, model):
-    y_rast_pred = cat_rast.predict(X_ML)
-    return y_rast_pred
